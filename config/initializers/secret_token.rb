1134aa4ea6b38b6a7034b03d2b0be2d7f823a685
Mary Wong <pair+mwong@modcloth.com>
2012-08-25 00:25:26 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamClient::Application.config.secret_token = '78c12dfa46b99f923a5f11c8f87618708e2fcbb0539df5da4b0fd7b9000f84882d583d7875a6e431cac14c72e3556cc6467e65e2481ef667e46b5006e257278c'
